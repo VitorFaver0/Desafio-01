62d313dbe99927fd8f710d5f97303c5f819b450c
VitorFaver0 <88974981+VitorFaver0@users.noreply.github.com>
2023-02-04 13:04:14 -0300

    resolucao desafio 01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,36 @@ interface Task {
   isComplete: boolean;
 }
 
+function isEmptyOrSpaces(str:string){
+  return str === null || str.match(/^ *$/) !== null;
+}
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!isEmptyOrSpaces(newTaskTitle)){
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      };
+
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => (task.id === id? {...task, isComplete:!task.isComplete } : task)));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task) => task.id!== id));
   }
 
   return (
